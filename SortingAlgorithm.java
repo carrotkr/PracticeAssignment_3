0ef8bea4798dfd48a34724a5e27c50902c575149
Kibeom Kim <31353801+carrotkr@users.noreply.github.com>
2017-09-18 21:02:57 -0700

    Update SortingAlgorithm.java

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -1,12 +1,20 @@
-
-
 /**
  * SortingAlgorithm is just a decorator for sorting.
  * (A real implementation would require a comparator and sort any object using a template.)
  * @author dgbrizan
- *
  */
 public interface SortingAlgorithm {
 
-	void sort(int [] a);
+	public void sort(int[] a);
+	
+	public static boolean findLessValue(int x, int y) {
+		return x < y;
+	}
+	
+	public static void exchangeValue(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
 }
