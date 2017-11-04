7d3c0514f825bc802161304129888b813608a585
Kibeom Kim <31353801+carrotkr@users.noreply.github.com>
2017-11-03 18:57:48 -0700

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,7 +2,16 @@
  * SelectionSort
  */
 public class SelectionSort implements SortingAlgorithm {
-
+	public void exchangeValue(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+	public boolean findLessValue(int x, int y) {
+		return x < y;
+	}
+	
 	@Override
 	public void sort(int[] a) {
 		int n = a.length;
@@ -11,13 +20,12 @@ public class SelectionSort implements SortingAlgorithm {
 			int min = i;
 			
 			for (int j = (i + 1); j < n; j++) {
-				if (SortingAlgorithm.findLessValue(a[j], a[min])) {
+				if (findLessValue(a[j], a[min])) {
 					min = j;
 				}
 			}
 			
-			SortingAlgorithm.exchangeValue(a, i, min);
+			exchangeValue(a, i, min);
 		}
 	}
-
 }
