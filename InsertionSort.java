615bd9f221a4a50274b6e3088a05608279f2fd2f
Kibeom Kim <31353801+carrotkr@users.noreply.github.com>
2017-11-03 18:57:00 -0700

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,16 +2,24 @@
  * InsertionSort
  */
 public class InsertionSort implements SortingAlgorithm {
+	public void exchangeValue(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+	public boolean findLessValue(int x, int y) {
+		return x < y;
+	}
 	
 	@Override
 	public void sort(int[] a) {
 		int n = a.length;
 		
 		for (int i = 1; i < n; i++) {
-			for (int j = i; j > 0 && SortingAlgorithm.findLessValue(a[j], a[j-1]); j--) {
-				SortingAlgorithm.exchangeValue(a, j, j-1);
+			for (int j = i; j > 0 && findLessValue(a[j], a[j-1]); j--) {
+				exchangeValue(a, j, j-1);
 			}
 		}
 	}
-	
 }
